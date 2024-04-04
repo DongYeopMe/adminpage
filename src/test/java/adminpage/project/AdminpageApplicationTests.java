71b798b8cc781c260d56eaf75ad9b2f212dcd845
Dongyeop Lee <dlduq29@gmail.com>
2024-04-05 00:37:57 +0900

    Initial commit

---
+++ src/test/java/adminpage/project/AdminpageApplicationTests.java
@@ -0,0 +1,13 @@
+package adminpage.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
